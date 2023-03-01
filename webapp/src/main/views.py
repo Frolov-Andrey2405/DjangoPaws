7584bd15a31d15c7c173539f10f4c4f05f917fb5
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-03-01 21:24:18 +0200

    home views

--- webapp/src/main/views.py
+++ webapp/src/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    # start a task
+    return HttpResponse('<h1>Uploading a dog image...</h1>')
