6997ec97801a783e123c00c060c33be8520d627f
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-03-01 21:59:56 +0200

    views tasks

--- webapp/src/main/views.py
+++ webapp/src/main/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from . import tasks
 
 # Create your views here.
 
 
 def home(request):
-    # start a task
+    """
+    The home function is responsible for uploading a dog image to the server.
+    It does this by calling the download_dog task, which downloads a random dog image from 
+    the internet and saves it in /tmp/dog.jpg on the server.
+
+    """
+    tasks.download_dog.delay()
     return HttpResponse('<h1>Uploading a dog image...</h1>')
