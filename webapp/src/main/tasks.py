b04ecaa3d3f03888e6f5f3383cfcf751e18a1084
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-03-01 21:45:49 +0200

    tasks

--- webapp/src/main/tasks.py
+++ webapp/src/main/tasks.py
@@ -1,4 +1,23 @@
-from celery import shared_task
+import requests as r
+import uuid
 
+from celery import shared_task
+from django.conf import settings
 
 # Create your tasks here
+
+DOG_URL = 'https://placedog.net/1080/1080?random'
+
+
+@shared_task
+def download_dog():
+    resp = r.get(DOG_URL)
+    file_ext = resp.headers.get('Content-Type').split('/')[1]
+    file_name = settings.BASE_DIR / 'dogs' / \
+        (str(uuid.uuid4()) + "." + file_ext)
+
+    with open(file_name, 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=128):
+            f.write(chunk)
+
+    return True
